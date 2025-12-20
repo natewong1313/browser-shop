596cb0dd86a5f51c920642e9297c66cb3df71969
natewong1313 <natewong1@gmail.com>
2025-12-20 12:43:39 -0600

    setup basic ws server

--- apps/browser-container/src/main.rs
+++ apps/browser-container/src/main.rs
@@ -1,46 +1,40 @@
+use anyhow::Context;
 use axum::{Router, routing::get};
 use chromiumoxide::browser::{Browser, BrowserConfig};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use futures::StreamExt;
 
 use crate::launcher::launch_browser;
 pub mod launcher;
+pub mod server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut browser_details = launch_browser().await?;
-    let mut browser = browser_details.browser;
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    // spawn a new task that continuously polls the handler
-    let handle = tokio::spawn(async move {
-        while let Some(h) = browser_details.handler.next().await {
-            if h.is_err() {
-                break;
-            }
-        }
-    });
+    server::serve().await.context("Server exploded").unwrap();
 
-    // // create a new browser page and navigate to the url
-    // let page = browser.new_page("https://en.wikipedia.org").await?;
+    // let mut browser_details = launch_browser().await?;
+    // let mut browser = browser_details.browser;
     //
-    // // find the search bar type into the search field and hit `Enter`,
-    // // this triggers a new navigation to the search result page
-    // page.find_element("input#searchInput")
-    //     .await?
-    //     .click()
-    //     .await?
-    //     .type_str("Rust programming language")
-    //     .await?
-    //     .press_key("Enter")
-    //     .await?;
+    // // spawn a new task that continuously polls the handler
+    // let handle = tokio::spawn(async move {
+    //     while let Some(h) = browser_details.handler.next().await {
+    //         if h.is_err() {
+    //             break;
+    //         }
+    //     }
+    // });
     //
-    // let html = page.wait_for_navigation().await?.content().await?;
-    //
-    // browser.close().await?;
-    handle.await?;
-    Ok(())
+    // handle.await?;
+    // Ok(())
+
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     //
     // let listener = tokio::net::TcpListener::bind("0.0.0.0:6700").await.unwrap();
     // axum::serve(listener, app).await.unwrap();
 }
+
